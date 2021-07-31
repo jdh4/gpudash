c8bd2f52331475bc3c685a0c54136e87fb6d5d0a
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-07-31 14:42:19 -0400

    Update a100.sh

--- a100.sh
+++ a100.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATA="/home/jdh4/bin/a100/data"
+DATA="/home/jdh4/bin/gpus/data"
 printf -v SECS '%(%s)T' -1
 
 curl -s 'http://vigilant.sn17:8480/api/v1/query?query=nvidia_gpu_duty_cycle' > ${DATA}/util.${SECS}
@@ -9,4 +9,4 @@ curl -s 'http://vigilant.sn17:8480/api/v1/query?query=nvidia_gpu_jobId'      > $
 
 find ${DATA} -type f -mmin +70 -exec rm -f {} \;
 
-/usr/licensed/anaconda3/2020.11/bin/python /home/jdh4/bin/a100/extract.py
+/usr/licensed/anaconda3/2020.11/bin/python /home/jdh4/bin/gpus/extract.py
