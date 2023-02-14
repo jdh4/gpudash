dbba17e36a08ce81bec33a0ba3a26f8f19d60546
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-02-14 17:21:51 -0500

    Update a100.sh

--- a100.sh
+++ a100.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+  
 DATA="/home/jdh4/bin/gpus/data"
 printf -v SECS '%(%s)T' -1
 
-curl -s 'http://vigilant.sn17:8480/api/v1/query?query=nvidia_gpu_duty_cycle' > ${DATA}/util.${SECS}
-curl -s 'http://vigilant.sn17:8480/api/v1/query?query=nvidia_gpu_jobUid'     > ${DATA}/uid.${SECS}
-curl -s 'http://vigilant.sn17:8480/api/v1/query?query=nvidia_gpu_jobId'      > ${DATA}/jobid.${SECS}
+curl -s 'http://vigilant2.sn17:8480/api/v1/query?query=nvidia_gpu_duty_cycle' > ${DATA}/util.${SECS}
+curl -s 'http://vigilant2.sn17:8480/api/v1/query?query=nvidia_gpu_jobUid'     > ${DATA}/uid.${SECS}
+curl -s 'http://vigilant2.sn17:8480/api/v1/query?query=nvidia_gpu_jobId'      > ${DATA}/jobid.${SECS}
 
 find ${DATA} -type f -mmin +70 -exec rm -f {} \;
 
-/usr/licensed/anaconda3/2020.11/bin/python /home/jdh4/bin/gpus/extract.py
+/usr/licensed/anaconda3/2022.5/bin/python /home/jdh4/bin/gpus/extract.py
 
 timeout 5 scp -r /scratch/.gpudash della8:/scratch/
