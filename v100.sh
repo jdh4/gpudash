c8835df08a4d8f350e3f2351252d467b85d550c2
jdh4 <52128661+jdh4@users.noreply.github.com>
2023-02-14 17:23:18 -0500

    Update v100.sh

--- v100.sh
+++ v100.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+  
 DATA="/home/jdh4/bin/gpus/data"
 printf -v SECS '%(%s)T' -1
 
-curl -s 'http://vigilant.sn814:8480/api/v1/query?query=nvidia_gpu_duty_cycle' > ${DATA}/util.${SECS}
-curl -s 'http://vigilant.sn814:8480/api/v1/query?query=nvidia_gpu_jobUid'     > ${DATA}/uid.${SECS}
-curl -s 'http://vigilant.sn814:8480/api/v1/query?query=nvidia_gpu_jobId'      > ${DATA}/jobid.${SECS}
+curl -s 'http://vigilant2.sn814:8480/api/v1/query?query=nvidia_gpu_duty_cycle' > ${DATA}/util.${SECS}
+curl -s 'http://vigilant2.sn814:8480/api/v1/query?query=nvidia_gpu_jobUid'     > ${DATA}/uid.${SECS}
+curl -s 'http://vigilant2.sn814:8480/api/v1/query?query=nvidia_gpu_jobId'      > ${DATA}/jobid.${SECS}
 
 find ${DATA} -type f -mmin +70 -exec rm -f {} \;
 
